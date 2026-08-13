99fb8b769bea8f3b24c6c34eda1914093a1ae232
tolley <chris.tolley@gmail.com>
2026-08-13 15:37:15 -0400

    Started a new front end in react and typescript

---
+++ pixels-front-end/pixels/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+import '../public/css/app.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div id="toolbar">
+        asdf
+      </div>
+    </>
+  )
+}
+
+export default App
